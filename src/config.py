e624ca9b98c744e0f8aeaa465a8dc86a4463fdbd
Serhii <kozlovsergiy2004@gmail.com>
2025-06-12 17:08:11 +0300

    added config

---
+++ src/config.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings
+
+
+class Networking(BaseModel):
+   host: str = "127.0.0.1"
+   port: int = 8000
+
+
+class Settings(BaseSettings):
+    networking: Networking
