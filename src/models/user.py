38825a8689c9a70750d9d00dbe79d9917bfe0d60
Serhii <kozlovsergiy2004@gmail.com>
2025-06-14 20:20:04 +0300

    moved folder models to database

--- src/models/user.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy.orm import Mapped, mapped_column
-
-from models import Base
-
-
-class User(Base):
-    email: Mapped[str] = mapped_column(unique=True)
-    username: Mapped[str] = mapped_column(unique=True)
