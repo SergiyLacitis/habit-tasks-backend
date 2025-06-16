12b0947925cd3dbf5b2b9e2b10fb97d3a404fcde
Serhii <kozlovsergiy2004@gmail.com>
2025-06-16 22:27:16 +0300

    updated Base model

--- src/database/models/base.py
+++ src/database/models/base.py
@@ -14,6 +14,6 @@ class Base(DeclarativeBase):
 
     @declared_attr.directive
     def __tablename__(cls) -> str:
-        return camel_case_to_snake_case(cls.__name__)
+        return f"{camel_case_to_snake_case(cls.__name__)}s"
 
     id: Mapped[int] = mapped_column(primary_key=True)
