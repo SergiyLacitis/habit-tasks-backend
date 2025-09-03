93990f335f339b279be508116ef4d54fcd373d5c
Serhii <kozlovsergiy2004@gmail.com>
2025-09-03 20:12:51 +0300

    changed User model

--- src/database/models/user.py
+++ src/database/models/user.py
@@ -1,3 +1,4 @@
+from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
@@ -7,3 +8,4 @@ from .mixins import IntIDPkMixin
 class User(Base, IntIDPkMixin):
     email: Mapped[str] = mapped_column(unique=True)
     username: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[str] = mapped_column(String(128), nullable=False)
