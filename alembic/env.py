65ad1365c064d990bb57a6dc5d60c2b8524203f1
Serhii <kozlovsergiy2004@gmail.com>
2025-12-07 20:31:51 +0200

    reinit alembic

--- alembic/env.py
+++ alembic/env.py
@@ -1,13 +1,13 @@
 import asyncio
 from logging.config import fileConfig
 
+from alembic import context
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
-from alembic import context
-from database import url
-from database.models import Base
+from habit_tasks.database import url
+from habit_tasks.database.models import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
